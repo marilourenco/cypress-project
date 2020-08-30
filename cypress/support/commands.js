8aad65b1d6d7508a6eecb185a804d74b138d7d1f
Marilene Lourenco <malourenco@MacBook-Pro-de-Marilene.local>
2020-08-30 15:57:24 -0300

    adding register spec for e2e tests using chanceJS

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,40 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('register', () => {
+    cy.visit('Register.html')
+        cy.get('input[placeholder="First Name"]').type(chance.first());
+        cy.get('input[ng-model="LastName"]').type(chance.last());
+        cy.get('input[ng-model="EmailAdress"]').type(chance.email());
+        cy.get('input[ng-model="Phone"]').type(chance.phone({formatted: false}));
+        cy.get('input[value="FeMale"]').check();
+        cy.get('input[type="checkbox"]').check('Movies');
+        cy.get('input[type="checkbox"]').check('Hockey');
+        cy.get('select#Skills').select('Java');
+        cy.get('select#countries').select('Brazil');
+        cy.get('select#country').select('Australia', {force: true});
+        cy.get('select#yearbox').select('1987');
+        cy.get('select[ng-model="monthbox"]').select('June');
+        cy.get('select#daybox').select('23');
+        cy.get('input#firstpassword').type('Senha123@');
+        cy.get('input#secondpassword').type('Senha123@');
+        cy.get('input#imagesrc').attachFile('upload-image-register.png');
+        cy.get('button#submitbtn').click();
+    });
+
+    Cypress.Commands.add('startServer', () => {
+        cy.server();
+    });
+
+    Cypress.Commands.add('routePostNewtabl', () => {
+        cy.route('POST', '**/api/1/databases/userdetails/collections/newtable?**').as('postNewtable');
+    });
+
+    Cypress.Commands.add('routePostUsertable', () => {
+        cy.route('POST', '**/api/1/databases/userdetails/collections/usertable?**').as('postUsertable');
+        });
+
+    Cypress.Commands.add('routeGetNewtable', () => {
+        cy.route('GET', '**/api/1/databases/userdetails/collections/newtable?**').as('getNewtable');
+            });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,11 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-file-upload'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+});
